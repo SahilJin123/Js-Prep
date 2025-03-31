4f971283563e473d62720bacd3ed6de9733dde34
Sahil Jindal <107670763+SahilJin123@users.noreply.github.com>
2025-03-31 17:36:02 +0000

    Lec 7

---
+++ 01_basics/04_StringConversions.js
@@ -0,0 +1,15 @@
+
+let str1 = "Hello"
+
+let str2 = " Sahil"
+
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+
+console.log(+true)
+console.log(+"")
